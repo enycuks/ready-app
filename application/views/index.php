20381cb602ed0859a2db2e04b4baca39c8437ed3
enycuks <enycuks@gmail.com>
2022-03-17 22:03:08 +0700

    template new

--- application/views/index.php
+++ application/views/index.php
@@ -28,48 +28,126 @@
 					<div class="row">
 						<div class="col-sm-12">
 							<div class="card">
+
 								<div class="card-header">
-									<h5>Hello card</h5>
-									<span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+									<h5>Basic Form Inputs</h5>
+									<span>Add class of <code>.form-control</code> with <code>&lt;input&gt;</code> tag</span>
+									<div class="card-header-right"><i class="icofont icofont-spinner-alt-5"></i></div>
+
 									<div class="card-header-right">
-										<ul class="list-unstyled card-option" style="width: 35px;">
-											<li class=""><i class="icofont icofont-simple-left"></i></li>
-											<li><i class="icofont icofont-maximize full-card"></i></li>
-											<li><i class="icofont icofont-minus minimize-card"></i></li>
-											<li><i class="icofont icofont-refresh reload-card"></i></li>
-											<li><i class="icofont icofont-error close-card"></i></li>
-										</ul>
+										<i class="icofont icofont-spinner-alt-5"></i>
 									</div>
+
 								</div>
 								<div class="card-block">
-									<p>
-										"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-										enim ad minim veniam, quis nostrud exercitation ullamco laboris
-										nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-										in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-										nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-										sunt in culpa qui officia deserunt mollit anim id est laborum."
-										"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-										enim ad minim veniam, quis nostrud exercitation ullamco laboris
-										nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-										in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-										nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-										sunt in culpa qui officia deserunt mollit anim id est laborum."
-										"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-										enim ad minim veniam, quis nostrud exercitation ullamco laboris
-										nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-										in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-										nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-										sunt in culpa qui officia deserunt mollit anim id est laborum."
-									</p>
+									<h4 class="sub-title">Basic Inputs</h4>
+									<form>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Simple Input</label>
+											<div class="col-sm-10">
+												<input type="text" class="form-control">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Placeholder</label>
+											<div class="col-sm-10">
+												<input type="text" class="form-control" placeholder="Type your title in Placeholder">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Password</label>
+											<div class="col-sm-10">
+												<input type="password" class="form-control" placeholder="Password input">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Read only</label>
+											<div class="col-sm-10">
+												<input type="text" class="form-control" placeholder="You can't change me" readonly="">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Disable Input</label>
+											<div class="col-sm-10">
+												<input type="text" class="form-control" placeholder="Disabled text" disabled="">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Predefine
+												Input</label>
+											<div class="col-sm-10">
+												<input type="text" class="form-control" value="Enter yout content after me">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Select Box</label>
+											<div class="col-sm-10">
+												<select name="select" class="form-control">
+													<option value="opt1">Select One Value Only</option>
+													<option value="opt2">Type 2</option>
+													<option value="opt3">Type 3</option>
+													<option value="opt4">Type 4</option>
+													<option value="opt5">Type 5</option>
+													<option value="opt6">Type 6</option>
+													<option value="opt7">Type 7</option>
+													<option value="opt8">Type 8</option>
+												</select>
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Round Input</label>
+											<div class="col-sm-10">
+												<input type="text" class="form-control form-control-round" placeholder=".form-control-round">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Maximum
+												Length</label>
+											<div class="col-sm-10">
+												<input type="text" class="form-control" placeholder="Content must be in 6 characters" maxlength="6">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Disable
+												Autocomplete</label>
+											<div class="col-sm-10">
+												<input type="text" class="form-control" placeholder="Autocomplete Off" autocomplete="off">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Static Text</label>
+											<div class="col-sm-10">
+												<div class="form-control-static">Hello !... This is
+													static text
+												</div>
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Color</label>
+											<div class="col-sm-10">
+												<input type="color" class="form-control">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Upload File</label>
+											<div class="col-sm-10">
+												<input type="file" class="form-control">
+											</div>
+										</div>
+										<div class="form-group row">
+											<label class="col-sm-2 col-form-label">Textarea</label>
+											<div class="col-sm-10">
+												<textarea rows="5" cols="5" class="form-control" placeholder="Default textarea"></textarea>
+											</div>
+										</div>
+									</form>
+
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+
 			</div>
 		</div>
 		<div id="styleSelector">
