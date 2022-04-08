fab0716e727dc9c507f9a0ddc11251e7c3d14ca5
ceboset <siemail.mne@gmail.com>
2022-04-08 15:31:53 +0700

    input data
    input data fix

--- ready.sql
+++ ready.sql
@@ -1,98 +1,110 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 21 Mar 2022 pada 14.43
--- Versi server: 10.4.18-MariaDB
--- Versi PHP: 7.3.27
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.33 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Database: `ready`
---
 
--- --------------------------------------------------------
+-- Dumping database structure for ready
+CREATE DATABASE IF NOT EXISTS `ready` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `ready`;
 
---
--- Struktur dari tabel `instansi`
---
+-- Dumping structure for table ready.data_pelapor
+CREATE TABLE IF NOT EXISTS `data_pelapor` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `penyidik` int(5) DEFAULT NULL,
+  `nama_tersangka` varchar(255) DEFAULT NULL,
+  `pasal` varchar(255) DEFAULT NULL,
+  `jpu` int(5) DEFAULT NULL,
+  `kasi` int(5) DEFAULT NULL,
+  `aspidum` int(5) DEFAULT NULL,
+  `tgl` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `instansi` (
-  `id` int(5) NOT NULL,
-  `nama` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Dumping data for table ready.data_pelapor: ~0 rows (approximately)
+/*!40000 ALTER TABLE `data_pelapor` DISABLE KEYS */;
+INSERT INTO `data_pelapor` (`id`, `penyidik`, `nama_tersangka`, `pasal`, `jpu`, `kasi`, `aspidum`, `tgl`) VALUES
+	(1, 7, 'Asrul', '30 ayat 1', 3, 4, 3, '2022-04-09');
+/*!40000 ALTER TABLE `data_pelapor` ENABLE KEYS */;
 
---
--- Dumping data untuk tabel `instansi`
---
-
-INSERT INTO `instansi` (`id`, `nama`) VALUES
-(1, 'DEWI FITRIANA'),
-(3, 'NADYA WATI'),
-(4, 'INDRA WIJAYA aaaaaaa');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `jaksa`
---
-
-CREATE TABLE `jaksa` (
-  `id` int(5) NOT NULL,
+-- Dumping structure for table ready.instansi
+CREATE TABLE IF NOT EXISTS `instansi` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
   `nama` varchar(50) NOT NULL,
-  `email` varchar(255) NOT NULL
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table ready.instansi: ~3 rows (approximately)
+/*!40000 ALTER TABLE `instansi` DISABLE KEYS */;
+INSERT INTO `instansi` (`id`, `nama`) VALUES
+	(1, 'DEWI FITRIANA'),
+	(3, 'NADYA WATI'),
+	(4, 'INDRA WIJAYA aaaaaaa');
+/*!40000 ALTER TABLE `instansi` ENABLE KEYS */;
+
+-- Dumping structure for table ready.satker_jaksa
+CREATE TABLE IF NOT EXISTS `satker_jaksa` (
+  `id` int(5) NOT NULL,
+  `satker` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `jaksa`
---
+-- Dumping data for table ready.satker_jaksa: ~16 rows (approximately)
+/*!40000 ALTER TABLE `satker_jaksa` DISABLE KEYS */;
+INSERT INTO `satker_jaksa` (`id`, `satker`) VALUES
+	(1, 'Kejaksaan Tinggi Kalimantan Tengah'),
+	(2, 'Kejaksaan Negeri Palangka Raya'),
+	(3, 'Kejaksaan Negeri Pulang Pisau'),
+	(4, 'Kejaksaan Negeri Kapuas'),
+	(5, 'Kejaksaan Negeri Kotawaringin Timur'),
+	(6, 'Kejaksaan Negeri Kotawaringin Barat'),
+	(7, 'Kejaksaan Negeri Katingan'),
+	(8, 'Kejaksaan Negeri Gunung Mas'),
+	(9, 'Kejaksaan Negeri Seruyan'),
+	(10, 'Kejaksaan Negeri Sukamara'),
+	(11, 'Kejaksaan Negeri Lamandau'),
+	(12, 'Kejaksaan Negeri Barito Selatan'),
+	(13, 'Kejaksaan Negeri Barito Timur'),
+	(14, 'Kejaksaan Negeri Barito Utara'),
+	(15, 'Kejaksaan Negeri Murung Raya'),
+	(16, 'Cabang Kejaksaan Negeri Palingkau di Kapuas');
+/*!40000 ALTER TABLE `satker_jaksa` ENABLE KEYS */;
 
-INSERT INTO `jaksa` (`id`, `nama`, `email`) VALUES
-(2, 'uhuy', 'uhuy122@gmail.com');
+-- Dumping structure for table ready.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) DEFAULT NULL,
+  `nip` text,
+  `jabatan` varchar(255) DEFAULT NULL,
+  `satker` int(5) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `no_hp` varchar(100) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `role` enum('1','2','3','4','5','6','7') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+-- Dumping data for table ready.user: ~7 rows (approximately)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `nama`, `nip`, `jabatan`, `satker`, `email`, `no_hp`, `password`, `role`) VALUES
+	(1, 'Fendi Admin', '196305041991032002', 'Admin', 1, 'fendcuk@gmail.com', '123', 'aduh', '1'),
+	(2, 'Fendi Operator', '123333', 'Operator', 2, 'fencuk@gmail.com', '123', 'aduh', '2'),
+	(3, 'Fendi JPU', '123333', 'JPU', 1, 'fencuk@gmail.com', '123', 'aduh', '3'),
+	(4, 'Fendi KASI', '123333', 'KASI', 4, 'fencuk@gmail.com', '123', 'aduh', '4'),
+	(5, 'Fendi Wakajati', '123333', 'Wakajati', 3, 'fencuk@gmail.com', '123', 'aduh', '5'),
+	(6, 'Fendi Penyidik', '123333', 'Penyiduk', 4, 'fencuk@gmail.com', '123', 'aduh', '6'),
+	(7, 'Fendi JPU', '123333', 'Aspidum', 2, 'fencuk@gmail.com', '123', 'aduh', '6');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
---
--- Indeks untuk tabel `instansi`
---
-ALTER TABLE `instansi`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `jaksa`
---
-ALTER TABLE `jaksa`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `instansi`
---
-ALTER TABLE `instansi`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT untuk tabel `jaksa`
---
-ALTER TABLE `jaksa`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
